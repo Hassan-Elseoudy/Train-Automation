51ce74e240a912d6cdb5195a76b99ec3ad7142dc
Hassan El-seoudy <31868364+Hassan-Elseoudy@users.noreply.github.com>
2018-12-10 13:29:18 +0200

    - A screenshot, header, caltrain, runner are added.

---
+++ caltrain.c
@@ -0,0 +1,44 @@
+#include "caltrain.h"
+
+void station_init(struct station *station)
+{
+    station->waitingRobots = 0;
+    station->emptySeats = 0;
+    station->leavingRobots = 0;
+    pthread_mutex_init(&(station->lock), NULL);
+    pthread_cond_init(&(station->launchOnBoard), NULL);
+    pthread_cond_init(&(station->trainArrived), NULL);
+}
+
+void station_load_train(struct station *station, int count)
+{
+    pthread_mutex_lock(&(station->lock));
+    station->emptySeats = count;
+    pthread_cond_broadcast(&(station->trainArrived));
+    while ((station->waitingRobots > 0 && station-> emptySeats > 0) || station->leavingRobots > 0)
+        pthread_cond_wait(&(station->launchOnBoard), &(station->lock));
+    station->emptySeats = 0;
+    pthread_mutex_unlock(&(station->lock));
+}
+
+void station_wait_for_train(struct station *station)
+{
+    pthread_mutex_lock(&(station->lock));
+    station->waitingRobots++;
+    while(!station->emptySeats)
+        pthread_cond_wait(&(station->trainArrived), &(station->lock));
+    if(station->emptySeats--)
+    {
+        station->waitingRobots--;
+        station->leavingRobots++;
+    }
+    pthread_mutex_unlock(&(station->lock));
+}
+
+void station_on_board(struct station *station)
+{
+    pthread_mutex_lock(&(station->lock));
+    if(station->leavingRobots--)
+        pthread_cond_signal(&(station->launchOnBoard));
+    pthread_mutex_unlock(&(station->lock));
+}
